b5efcd7370360cb11efc35902cc2b2836eccf13e
arefev.ad <anton.arefiev@alp-erp.ru>
2025-04-26 23:13:20 +0300

    file: tests step

--- internal/client/tui/step/step_test.go
+++ internal/client/tui/step/step_test.go
@@ -384,4 +384,80 @@ func TestStep(t *testing.T) {
 
 		require.NotEmpty(t, buf.Len())
 	})
+
+	t.Run("lk list step success", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		var buf bytes.Buffer
+		var in bytes.Buffer
+		in.Write([]byte("q"))
+
+		data := &[]model.MetaListData{
+			{
+				UUID: "uuid_test",
+				Type: "type_test",
+				Name: "name_test",
+				File: "file_test",
+				Date: "date_test",
+			},
+		}
+
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Millisecond*100)
+		defer cancel()
+
+		l, err := logger.Build("debug")
+		require.NoError(t, err)
+
+		conn := mock_app.NewMockConnection(ctrl)
+		conn.EXPECT().CheckTokenCmd().MinTimes(1).MaxTimes(2)
+		conn.EXPECT().GetList(gomock.Any()).MinTimes(1).MaxTimes(1).Return(data, nil)
+		app := app.NewApp(conn, l)
+
+		p := tea.NewProgram(NewLKList(app), tea.WithInput(&in), tea.WithOutput(&buf), tea.WithContext(ctx))
+		if _, err := p.Run(); err != nil {
+			require.Contains(t, err.Error(), "context deadline exceeded")
+		}
+
+		require.NotEmpty(t, buf.Len())
+	})
+
+	t.Run("delete action step success", func(t *testing.T) {
+		const uuid string = "uuid_test"
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		var buf bytes.Buffer
+		var in bytes.Buffer
+		in.Write([]byte("q"))
+
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Millisecond*100)
+		defer cancel()
+
+		data := &[]model.MetaListData{
+			{
+				UUID: "uuid_test",
+				Type: "type_test",
+				Name: "name_test",
+				File: "file_test",
+				Date: "date_test",
+			},
+		}
+
+		l, err := logger.Build("debug")
+		require.NoError(t, err)
+
+		conn := mock_app.NewMockConnection(ctrl)
+		conn.EXPECT().CheckTokenCmd().MinTimes(1).MaxTimes(2)
+		conn.EXPECT().Delete(gomock.Any(), uuid).MinTimes(1).MaxTimes(1).Return(nil)
+		conn.EXPECT().GetList(gomock.Any()).MinTimes(1).MaxTimes(1).Return(data, nil)
+		app := app.NewApp(conn, l)
+
+		p := tea.NewProgram(NewDeleteAction(uuid, app), tea.WithInput(&in), tea.WithOutput(&buf), tea.WithContext(ctx))
+		if _, err := p.Run(); err != nil {
+			require.Contains(t, err.Error(), "context deadline exceeded")
+		}
+
+		require.NotEmpty(t, buf.Len())
+	})
 }
