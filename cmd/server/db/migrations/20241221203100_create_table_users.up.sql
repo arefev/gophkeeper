a5ab1e7d0a704e5184ba2513c8c3ee31c4003b6d
arefev.ad <anton.arefiev@alp-erp.ru>
2025-05-05 10:33:17 +0300

    file: set max length for string in users table

--- cmd/server/db/migrations/20241221203100_create_table_users.up.sql
+++ cmd/server/db/migrations/20241221203100_create_table_users.up.sql
@@ -1,8 +1,8 @@
 BEGIN;
 CREATE TABLE IF NOT EXISTS public.users (
     id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-    "login" varchar NOT NULL,
-    "password" varchar NOT NULL,
+    "login" varchar(255) NOT NULL,
+    "password" varchar(400) NOT NULL,
     "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT users_pk PRIMARY KEY (id),
